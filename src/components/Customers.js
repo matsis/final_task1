5ad3d888d4151a6884294342b75de37e25369fe9
Matti <matsis71@gmail.com>
2021-04-12 21:59:19 +0300

    task1 fix

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect} from 'react';
 import { AgGridReact } from 'ag-grid-react';
-import moment from 'moment';
-import 'moment/locale/fi';
-
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
